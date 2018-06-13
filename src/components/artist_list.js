b2f2fbe107962d1583c9f114edee68c17b2cc78a
toby-adedipe <toby_adedipe@yahoo.com>
2018-06-13 15:31:31 +0100

    added styling to homepage and rendered component banner and rendered artist list

---
+++ src/components/artist_list.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ArtistsList = (props) =>{
+
+    console.log(props);
+    return(
+        <div> List </div>
+    )
+
+}
+
+export default ArtistsList;
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
+import Banner from '../components/banner';
+import ArtistsList from './artist_list';
+
+const URL_ARTISTS = 'http://localhost:3004/artists'
+
 class Home extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            artists:''
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL_ARTISTS, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                artists:json
+            })
+        })
+    }
+
     render(){
         return(
-            <div>Home</div>
+            <div>
+                <Banner />
+                <ArtistsList allArtist={this.state.artists} />
+            </div>
         )
     }
 }
