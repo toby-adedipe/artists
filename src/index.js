99c5d8cfd379207347c96338c4393bb997a03390
Adedipe Toby <toby_adedipe@yahoo.com>
2019-01-01 13:46:10 +0100

    removed external database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
-
+import * as serviceWorker from './registerServiceWorker';
 //components
 
 import Home from './components/home'
@@ -19,3 +19,4 @@ const App = () =>{
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
